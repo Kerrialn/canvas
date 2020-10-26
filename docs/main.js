4c48ef6d8b4f830f3ebde17d206d968f24da7f74
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-10-26 18:33:53 +0100

    update features

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const canvas = document.querySelector('#canvas');
 const circleSize = document.querySelector('#circleSize');
 const randomize = document.querySelector('#randomize');
@@ -9,15 +10,40 @@ const alpha = document.querySelector('#alpha');
 const strokeOrFill = document.querySelector('#strokeOrFill');
 const colorMe = document.querySelector('#colorMe');
 
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  changeColor();
+});
 
-console.log(colorMe.value)
+const downloadImage = () => {
+let img = new Image();
+img.classList.add("col");
+img.classList.add("m4");
+img.classList.add("preview-image");
+
+img.src = canvas.toDataURL();
+document.querySelector('#preview-holder').appendChild(img);
+}
+
+
+const changeColor = () => {
+  red.value = Math.floor(Math.random() * 255);
+  green.value = Math.floor(Math.random() * 255);
+  blue.value = Math.floor(Math.random() * 255);
+  alpha.value = Math.random(0,1)
+
+  body.style.backgroundColor = 'rgba('+red.value+', '+green.value+', '+blue.value+', '+alpha.value+')'
+}
+
 
 let ctx = canvas.getContext('2d');
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 canvas.addEventListener('mousemove', (event)=>{
 
-  for (var i = 0; i < 6; i++) {
-    for (var j = 0; j < 6; j++) {
+  for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 2; j++) {
 
     let shape = new Path2D();
     let x = event.offsetX;
@@ -45,8 +71,12 @@ canvas.addEventListener('mousemove', (event)=>{
         if(colorMe.checked){
           ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' + 
           Math.floor(255 - 32.5 * j) + ')';
+
+          body.style.backgroundColor = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' + 
+          Math.floor(255 - 42.5 * j) + ')';
         }else{
           ctx.strokeStyle = 'rgba('+ r +', ' + g + ', ' + b + ', '+a+')';  
+          body.style.backgroundColor = 'rgba('+ r +', ' + g + ', ' + b + ', '+a+')'; 
         }
         ctx.stroke(shape);
       }
@@ -55,8 +85,13 @@ canvas.addEventListener('mousemove', (event)=>{
         if(colorMe.checked){
           ctx.fillStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' + 
           Math.floor(255 - 42.5 * j) + ')';
+
+          body.style.backgroundColor = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' + 
+          Math.floor(255 - 42.5 * j) + ')';
+
         }else{
           ctx.fillStyle = 'rgba('+ r +', ' + g + ', ' + b + ', '+a+')';  
+          body.style.backgroundColor = 'rgba('+ r +', ' + g + ', ' + b + ', '+a+')'; 
         }
         ctx.fill(shape);
       }
